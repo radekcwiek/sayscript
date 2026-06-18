5e9e47a46dacb72c789a51fb8b3fac3a5f6ee379
w074n <radek.cwiek@mail.de>
2026-06-18 18:43:51 +0200

    Windows Build Script modified

--- build.ps1
+++ build.ps1
@@ -1,11 +1,13 @@
 $ErrorActionPreference = "Stop"
 
-Write-Host "Cleaning old build files..."
-Remove-Item -Recurse -Force build, dist -ErrorAction SilentlyContinue
+Write-Host "Cleaning old build folders..."
+
+Remove-Item -Recurse -Force .\build -ErrorAction SilentlyContinue
+Remove-Item -Recurse -Force .\dist -ErrorAction SilentlyContinue
 
 Write-Host "Building SayScript..."
-pyinstaller SayScript.spec
 
-Write-Host "Build complete."
-Write-Host "EXE:"
-Write-Host ".\dist\SayScript\SayScript.exe"
+pyinstaller .\SayScript.spec
+
+Write-Host "Build finished."
+Write-Host "Output: dist\SayScript"
